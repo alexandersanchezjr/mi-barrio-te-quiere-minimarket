f869f86407d30cf4d21b0c8f8faefacd55a9e057
Alexander <alexssjr2002@gmail.com>
2021-04-05 14:04:30 -0500

    Attributes, getters and setters implemented

--- src/model/Minimarket.java
+++ src/model/Minimarket.java
@@ -1,5 +1,43 @@
 package model;
 
-public class Minimarket {
+import java.util.ArrayList;
 
+public class Minimarket {
+	private ArrayList<Person> peopleEntered;
+	private long counter;
+	/**
+	 * @param peopleEntered
+	 * @param counter
+	 */
+	public Minimarket() {
+		this.peopleEntered = new ArrayList <Person>();
+		this.counter = 0;
+	}
+	/**
+	 * @return the peopleEntered
+	 */
+	public ArrayList<Person> getPeopleEntered() {
+		return peopleEntered;
+	}
+	/**
+	 * @param peopleEntered the peopleEntered to set
+	 */
+	public void setPeopleEntered(ArrayList<Person> peopleEntered) {
+		this.peopleEntered = peopleEntered;
+	}
+	/**
+	 * @return the counter
+	 */
+	public long getCounter() {
+		return counter;
+	}
+	/**
+	 * @param counter the counter to set
+	 */
+	public void setCounter(long counter) {
+		this.counter = counter;
+	}
+	
+	
+	
 }
