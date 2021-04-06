12e08d3097a0341fdc4db0430737e65ae387cf58
Alexander <alexssjr2002@gmail.com>
2021-04-05 22:12:45 -0500

    @SuppressWarnings(serial) implemented

--- src/exceptions/SameParityException.java
+++ src/exceptions/SameParityException.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+@SuppressWarnings("serial")
 public class SameParityException extends Exception {
 	
 	private int secondLastNumber; 
--- src/exceptions/UnderAgeException.java
+++ src/exceptions/UnderAgeException.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+@SuppressWarnings("serial")
 public class UnderAgeException extends Exception {
 
 	public UnderAgeException() {
